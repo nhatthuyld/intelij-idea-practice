9f9a385a909ec3819bd9b12189ce871b0df9a932
thuy <nhatthuyld@hanatourjsv.com>
2020-05-07 16:19:23 +0700

    practise intelij

---
+++ src/test/java/package1/mangedriver.java
@@ -0,0 +1,33 @@
+package package1;
+
+public class mangedriver {
+    import org.openqa.selenium.WebDriver;
+    import org.openqa.selenium.chrome.ChromeDriver;
+    import org.openqa.selenium.support.ui.WebDriverWait;
+
+    public class DriverFactory {
+
+        private static WebDriver driver;
+        private static WebDriverWait wait;
+
+        private DriverFactory(){
+            // prevent instantiation
+        }
+
+        public static WebDriver getChromeDriver(){
+
+            if(driver == null){
+                System.setProperty("webdriver.chrome.driver","src/test/java/drivers/chromedriver");
+                driver = new ChromeDriver();
+            }
+            return driver;
+        }
+
+        public static WebDriverWait getWebDriverWait(){
+            if(wait == null){
+                wait = new WebDriverWait(getChromeDriver(), 5);
+            }
+            return wait;
+        }
+    }
+}
