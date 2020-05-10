e0a1a1e212456b4a670cf1cb2e6e87d54c5551c9
nhatthuy <nhatthuyld@gmail.com>
2020-05-10 16:47:29 +0700

    correct setup

---
+++ src/test/java/package1/MangeDriver.java
@@ -0,0 +1,6 @@
+package package1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MangeDriver {
+}
--- src/test/java/testng/MyFirstTest.java
+++ src/test/java/testng/MyFirstTest.java
@@ -1,10 +1,22 @@
 package testng;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import package1.DriverFactory;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MyFirstTest {
 
@@ -13,8 +25,13 @@ public class MyFirstTest {
 
     @BeforeMethod
     public void SetUp() {
-        System.setProperty("webdriver.chrome.driver", "resource/chromedriver");
-        driver = new ChromeDriver();
+        driver = DriverFactory.getChromeDriver();
+
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.close();
 
     }
 
@@ -22,7 +39,22 @@ public class MyFirstTest {
     public void firstTestNG() {
         driver.get("https://vnexpress.net/");
         // Assert
-        Assert.assertTrue(driver.getTitle().contains("vnexpress"));
+        Assert.assertTrue(driver.getTitle().contains("VnExpress"));
+    }
+
+    @Test(description = "Test I created for ")
+    public void takeScreenShort() throws IOException {
+        driver.get("https://vnexpress.net/");
+        TakesScreenshot tkss = (TakesScreenshot) driver;
+        File vnExpressHomePage = tkss.getScreenshotAs(OutputType.FILE);
+        Path outputScreenshot =  Paths.get("output/vn-express" + System.currentTimeMillis() + ".png");
+
+        //Copy file at destination
+        Files.copy(Paths.get(vnExpressHomePage.getPath()),outputScreenshot);
+
+        // ImageIO.write(screenshot.getImage(), "jpg", new File(".\\screenshot\\fullimage.jpg"));
+        // Assert
+        Assert.assertTrue(driver.getTitle().contains("VnExpress"));
     }
 
 }
